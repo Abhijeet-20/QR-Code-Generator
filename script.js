486c8fdf975ef588567681d04ead28ecf5224e5c
Abhijeet <132607217+Abhijeet-20@users.noreply.github.com>
2023-08-24 15:07:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let imgbox = document.getElementById("imgbox");
+        let qrimg1 = document.getElementById("qrimg1");
+        let url = document.getElementById("url");
+        
+        function generate(){
+            if(url.value.length > 0){
+            qrimg1.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + url.value;
+            imgbox.style.display="block";
+            }
+            else{
+                url.classList.add('error');
+                setTimeout(()=>{
+                    url.classList.remove('error');
+                },600);
+            }
+        }
